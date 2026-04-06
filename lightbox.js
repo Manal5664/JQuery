2c59044db55c57b474c005e222f29225bb487c38
Manal Asghar <manal.atif2021@gmail.com>
2026-04-06 15:34:25 +0500

    first commit

---
+++ lightbox.js
@@ -0,0 +1,10 @@
+
+        $(document).ready(function(){
+            $('.gallerys').magnificPopup({
+                delegate:'a',
+                type:'image',
+                gallery:{
+                enabled:true
+                }
+            });
+        });
